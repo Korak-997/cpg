7b83366291aae63dbc5dd567768dee5df1510382
dani-lp <dani.linux.97@gmail.com>
2021-01-24 11:39:55 +0100

    Adding dashboard

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="dashboard">
+    <h1>This is an Dashboard page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
