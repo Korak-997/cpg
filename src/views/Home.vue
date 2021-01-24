7a9927615735205f452be8f00153b0deea6ff6cd
dani-lp <dani.linux.97@gmail.com>
2021-01-24 14:02:06 +0100

    Adding inital homeppage elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,48 @@
 <template>
-  <div id="home">
-    <h1>This is an Home page</h1>
+  <div id="home" class="wraper">
+    <div class="main">
+      <div v-show="colors.length > 0" class="colors"></div>
+      <div @mouseover="changeBg()" id="canva">
+        <p>1) Move Your mouse around to generate colors.</p>
+        <p>2) Click to save the color to your pallete.</p>
+      </div>
+    </div>
+    <div class="controls">Controls will be here</div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      colors: [],
+    };
+  },
+  methods: {
+    changeBg() {
+      document.getElementById("canva").style.backgroundColor = "rgb(0, 0, 0)";
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
-
+.main {
+  width: 100%;
+  height: 90%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  #canva {
+    width: 95%;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+.controls {
+  width: 60%;
+  margin: 0.3em auto;
+}
 </style>
-
