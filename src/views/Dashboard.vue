99e9dbda1f4383c4865e02f78815fe28828d28d6
Dani-DESK <dani.linux.97@gmail.com>
2021-02-16 12:21:13 +0100

    Add contents in Dashboard page
    -In Dashboard page all the palletes for the user will be shown
    - User can delete the palletes from the Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,13 +1,61 @@
 <template>
-	<div id="dashboard">
-		<h1>This is an Dashboard page</h1>
-	</div>
+  <div id="dashboard" class="wraper">
+    <h1>Welcome to your Dashboard</h1>
+    <div v-for="pallete in palletes" :key="pallete" class="palletes">
+      <div class="colors">
+        <p v-for="color in pallete.colors" :key="color" class="color" >
+          <span>{{color}}</span>
+        </p>
+      </div>
+      <div class="controls">
+        <p>Here goes controlers</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	name: "Dashboard",
+  name: "Dashboard",
+  data() {
+    return{
+      palletes: []
+    }
+  },
+  created() {
+    this.palletes = JSON.parse(localStorage.getItem("palletes") || "[]");
+    this.$nextTick(() => {
+      document.querySelectorAll(".color").forEach((x) =>{
+        x.style.background = `${x.childNodes[0].innerText}`
+        x.childNodes[0].style.display = "none"
+      })
+    })
+  }
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+#dashboard{
+  text-align: center;
+  h1{
+    color:$dark-blue;
+  }
+  .palletes{
+    border: 2px solid red;
+    .controls{
+      border: 2px solid blue;
+    }
+    .colors{
+      border: 2px solid green;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      align-items: center;
+      p{
+        width: 20%;
+        height: 3rem;
+      }
+    }
+  }
+}
+</style>
